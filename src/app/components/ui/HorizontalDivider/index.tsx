214b04922027ab5e4765e219334f7417f2754572
NEVI0 <neviocostamagagnin@gmail.com>
2025-06-14 17:37:52 -0300

    feat: adding class prop to component

--- src/app/components/ui/HorizontalDivider/index.tsx
+++ src/app/components/ui/HorizontalDivider/index.tsx
@@ -1,7 +1,21 @@
+import { concatClasses } from '@app/helpers';
+
 interface HorizontalDividerProps {
   id?: string;
+  className?: string;
 }
 
-export default function HorizontalDivider({ id }: HorizontalDividerProps) {
-  return <div id={id} className="w-full h-[1px] bg-border rounded-full" />;
+export default function HorizontalDivider({
+  id,
+  className,
+}: HorizontalDividerProps) {
+  return (
+    <div
+      id={id}
+      className={concatClasses(
+        'w-full h-[1px] bg-border rounded-full',
+        className
+      )}
+    />
+  );
 }
