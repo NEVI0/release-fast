79af755a374acdb4461390719c214259e5b8d0e5
NEVI0 <neviocostamagagnin@gmail.com>
2025-06-19 22:17:45 -0300

    feat: add Badge component to Hero section for public beta notification

--- src/app/(pages)/(home)/_components/Hero/index.tsx
+++ src/app/(pages)/(home)/_components/Hero/index.tsx
@@ -1,12 +1,16 @@
 import Link from 'next/link';
 import { ArrowRight } from 'lucide-react';
 
-import { Button } from '@app/components/ui';
+import { Badge, Button } from '@app/components/ui';
 
 export default function Hero() {
   return (
     <section className="relative flex flex-col gap-8 items-center justify-center h-[600px]">
       <div className="flex flex-col gap-2 w-[80%] relative z-10">
+        <div className="flex items-center justify-center mb-4">
+          <Badge>🚀 Beta publico!</Badge>
+        </div>
+
         <h1 className="text-center font-bold text-4xl">
           Notifique seus usuários de forma{' '}
           <strong className="text-primary">clara</strong> e{' '}
