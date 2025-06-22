e0c3a0d7296a69a1fa42befb9be72aaf129a4775
NEVI0 <neviocostamagagnin@gmail.com>
2025-06-22 16:26:38 -0300

    refactor: simplify Card component by removing variant prop and adding children prop for flexible content rendering

--- src/app/components/common/Card/index.tsx
+++ src/app/components/common/Card/index.tsx
@@ -1,35 +1,29 @@
-import { concatClasses } from '@app/helpers';
-
-type CardVariant =
-  | 'default'
-  | 'primary'
-  | 'secondary'
-  | 'success'
-  | 'warning'
-  | 'error';
-
 interface CardProps {
   title: string;
   value: string;
   icon: React.ElementType;
-  variant?: CardVariant;
+  children?: React.ReactNode;
 }
 
 export default function Card({
   title,
   value,
   icon: Icon,
-  variant = 'default',
+  children,
 }: CardProps) {
   return (
-    <div className="bg-container border border-border rounded-2xl px-8 py-6 flex items-center justify-between w-full">
-      <div className="flex flex-col">
-        <p className="text-sm text-text-secondary">{title}</p>
-        <p className="text-2xl font-semibold">{value}</p>
+    <div className="bg-container border border-border rounded-2xl flex flex-col px-8 py-6 gap-6 w-full">
+      <div className="w-[48px] h-[48px] flex items-center justify-center rounded-xl bg-primary/10">
+        <Icon className="text-primary" />
       </div>
 
-      <div className="w-[48px] h-[48px] flex items-center justify-center rounded-2xl bg-primary/10">
-        <Icon className=" text-primary" />
+      <div className="flex items-center justify-between">
+        <div className="flex flex-col">
+          <p className="text-lg text-text-secondary">{title}</p>
+          <p className="text-2xl font-semibold">{value}</p>
+        </div>
+
+        <div>{children}</div>
       </div>
     </div>
   );
