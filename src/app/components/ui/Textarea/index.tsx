ed08ab1e05476b77e6f87611e54ada1c8f9b5d33
NEVI0 <neviocostamagagnin@gmail.com>
2025-06-23 22:19:03 -0300

    feat: add Textarea component for enhanced text input functionality and update index export

---
+++ src/app/components/ui/Textarea/index.tsx
@@ -0,0 +1,70 @@
+import { concatClasses } from '@app/helpers';
+
+interface TextareaProps
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
+  id: string;
+  label?: string;
+  error?: string;
+  disabled?: boolean;
+  className?: string;
+  classNameInput?: string;
+  icon?: React.ElementType;
+
+  rightButton?: React.ReactNode;
+}
+
+export default function Textarea({
+  id,
+  label,
+  error,
+  disabled,
+  className,
+  classNameInput,
+  icon: Icon,
+  rightButton,
+  ...props
+}: TextareaProps) {
+  return (
+    <div className={concatClasses('flex flex-col gap-2', className)}>
+      {label && (
+        <div className="flex items-center justify-between">
+          <label htmlFor={id} className="flex items-center gap-2">
+            {label}
+            {props.required && <span className="text-red-600">*</span>}
+          </label>
+
+          <div className="flex items-center gap-2">{rightButton}</div>
+        </div>
+      )}
+
+      <div
+        className={concatClasses(
+          ' px-6 py-4 rounded-xl flex items-start gap-2 disabled:cursor-not-allowed bg-container border border-border hover:not-focus-within:border-border-action focus-within:border-primary transition-colors',
+          !!error && 'border-red-600',
+          classNameInput
+        )}
+      >
+        <textarea
+          {...props}
+          id={id}
+          disabled={disabled}
+          className="min-h-[200px] w-full text-text-primary placeholder:text-text-secondary"
+        />
+
+        {!!Icon && (
+          <Icon
+            className={concatClasses(
+              'size-5 text-primary',
+              disabled && 'text-text-secondary',
+              !!error && 'text-red-600'
+            )}
+          />
+        )}
+      </div>
+
+      {!!error && (
+        <span className="text-red-600 text-sm font-medium">{error}</span>
+      )}
+    </div>
+  );
+}
--- src/app/components/ui/index.ts
+++ src/app/components/ui/index.ts
@@ -7,4 +7,5 @@ export { default as Input } from './Input';
 export { default as Modal } from './Modal';
 export { default as Select } from './Select';
 export { default as Switch } from './Switch';
+export { default as Textarea } from './Textarea';
 export { default as VerticalDivider } from './VerticalDivider';
