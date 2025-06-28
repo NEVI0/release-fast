c25585894379f342f402244731262b42f7c89d4b
NEVI0 <neviocostamagagnin@gmail.com>
2025-06-28 16:22:44 -0300

    fix: handle missing provider in accessAccountAction by defaulting to 'google'

--- src/app/actions/accessAccountAction.ts
+++ src/app/actions/accessAccountAction.ts
@@ -4,11 +4,11 @@ import { signIn, signOut, auth } from '@configs/auth';
 
 type Provider = 'google' | 'github' | 'gitlab';
 
-export default async function accessAccountAction(provider: Provider) {
+export default async function accessAccountAction(provider?: Provider) {
   const session = await auth();
 
   if (!session) {
-    return await signIn(provider, { redirectTo: '/dash' });
+    return await signIn(provider || 'google', { redirectTo: '/dash' });
   }
 
   return await signOut({ redirectTo: '/' });
