e2e09c5a90030221ac547a80ee8f37f51f5df322
NEVI0 <neviocostamagagnin@gmail.com>
2025-06-28 16:22:27 -0300

    feat: implement middleware for session-based redirection between authentication and dashboard routes

---
+++ src/middleware.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+import { auth } from '@configs/auth';
+import { getAppUrl } from '@app/helpers';
+
+export async function middleware(request: NextRequest) {
+  const session = await auth();
+  const pathname = request.nextUrl.pathname;
+
+  if (pathname.startsWith('/auth') && session) {
+    return NextResponse.redirect(new URL(getAppUrl('/dash')));
+  }
+
+  if (pathname.startsWith('/dash') && !session) {
+    return NextResponse.redirect(new URL(getAppUrl('/auth')));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'], // Exclude API routes and static files
+};
