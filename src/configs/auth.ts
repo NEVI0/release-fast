3d489ce36217213177b9cb77785b37bbab6a2ff3
NEVI0 <neviocostamagagnin@gmail.com>
2025-06-28 18:12:20 -0300

    feat: add session and JWT callbacks to NextAuth configuration for enhanced user session management

--- src/configs/auth.ts
+++ src/configs/auth.ts
@@ -24,6 +24,22 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   session: {
     strategy: 'jwt',
   },
+  callbacks: {
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.sub as string;
+      }
+
+      return session;
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+
+      return token;
+    },
+  },
   providers: [
     // Credentials({
     //   credentials: {
