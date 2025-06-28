a09daba88c538562c684de6e76a4ea5a6f457bab
NEVI0 <neviocostamagagnin@gmail.com>
2025-06-28 17:47:18 -0300

    refactor: simplify createProjectAction by removing validation schema and directly using CreateProjectDTO

--- src/app/actions/createProjectAction.ts
+++ src/app/actions/createProjectAction.ts
@@ -1,20 +1,11 @@
 'use server';
 
-import { z } from 'zod';
-
 import { CreateProjectDTO } from '@domain/dtos';
 import { makeCreateProjectUseCase } from '@factories/useCases';
 import { handleError } from '@app/helpers';
 
-const schema = z.object({
-  name: z.string().min(1, 'Name is required'),
-  description: z.string().min(1, 'Description is required'),
-  userId: z.string().min(1, 'User ID is required'),
-});
-
-export default async function createProjectAction(params: CreateProjectDTO) {
+export default async function createProjectAction(dto: CreateProjectDTO) {
   try {
-    const dto = schema.parse(params);
     const project = await makeCreateProjectUseCase().execute(dto);
 
     return {
@@ -29,6 +20,7 @@ export default async function createProjectAction(params: CreateProjectDTO) {
       },
     };
   } catch (error) {
+    console.log(error);
     const { message } = handleError(error, 'createProjectAction');
 
     return {
