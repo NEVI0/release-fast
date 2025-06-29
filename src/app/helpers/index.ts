17c12631000480cd57c6e9f58c7a9d657b569a87
NEVI0 <neviocostamagagnin@gmail.com>
2025-06-29 11:27:53 -0300

    feat: export formatDate and generateRandomUUID helper functions from index for improved utility

--- src/app/helpers/index.ts
+++ src/app/helpers/index.ts
@@ -1,4 +1,6 @@
 export { default as concatClasses } from './concatClasses';
+export { default as formatDate } from './formatDate';
 export { default as formatToCurrency } from './formatToCurrency';
+export { default as generateRandomUUID } from './generateRandomUUID';
 export { default as getAppUrl } from './getAppUrl';
 export { default as handleError } from './handleError';
