a2fca209e25d95a64ec933ff291c1228d6649ddc
NEVI0 <neviocostamagagnin@gmail.com>
2025-06-29 12:56:01 -0300

    feat: enhance project list loading experience with LoadingList component and integrate suspense for improved UI

--- src/app/(pages)/dash/projects/_components/List/index.tsx
+++ src/app/(pages)/dash/projects/_components/List/index.tsx
@@ -1,82 +1,85 @@
 import Link from 'next/link';
-import { Suspense } from 'react';
 import { ChevronRight, Plus } from 'lucide-react';
 
 import { SessionAbstract } from '@domain/entities';
 
-import { formatDate, generateRandomUUID } from '@app/helpers';
+import { formatDate } from '@app/helpers';
 import { fetchAllProjectsAction } from '@app/actions';
 
 import { Button, IconButton, Table } from '@app/components/ui';
+import { ErrorStatus } from '@app/components/common';
 
 interface ListProps {
   session: SessionAbstract;
+  columns: any;
 }
 
-export default async function List({ session }: ListProps) {
-  const { projects, success } = await fetchAllProjectsAction({
+export default async function List({ session, columns }: ListProps) {
+  const { projects } = await fetchAllProjectsAction({
     userId: session.user.id,
   });
 
-  if (!success) {
-    return <section className="flex flex-col gap-4">Error</section>;
+  if (!projects.length) {
+    return (
+      <ErrorStatus
+        title="Ops... nada encontrado!"
+        message="Não foi encontrado nenhum projeto cadastrado. Tente cadastrar um novo clicando no botão abaixo."
+      >
+        <Link href="/dash/projects/create">
+          <Button variant="primary">
+            Adicionar novo projeto
+            <Plus className="size-5" />
+          </Button>
+        </Link>
+      </ErrorStatus>
+    );
   }
 
-  const columns = [
-    { id: generateRandomUUID(), children: 'Nome' },
-    { id: generateRandomUUID(), children: 'Descrição' },
-    { id: generateRandomUUID(), children: 'Criação em' },
-    { id: generateRandomUUID(), children: 'Última atualização em' },
-    { id: generateRandomUUID(), children: 'Detalhes', center: true },
-  ];
-
   return (
-    <Suspense fallback={<section>Carregando...</section>}>
-      <section className="flex flex-col gap-4">
-        <div className="flex items-center justify-between">
-          <div>
-            <h3 className="text-lg font-semibold">Listagem de projetos</h3>
+    <section className="flex flex-col gap-4">
+      <div className="flex items-center justify-between">
+        <div>
+          <h3 className="text-lg font-semibold">Listagem de projetos</h3>
 
-            <h4 className="text-text-secondary">
-              Total de projetos: {projects.length}
-            </h4>
-          </div>
-
-          <Link href="/dash/projects/create">
-            <Button>
-              Adicionar novo projeto
-              <Plus className="size-5" />
-            </Button>
-          </Link>
+          <h4 className="text-text-secondary">
+            Total de projetos: {projects.length}
+          </h4>
         </div>
 
-        <Table>
-          <Table.Head columns={columns} />
+        <Link href="/dash/projects/create">
+          <Button>
+            Adicionar novo projeto
+            <Plus className="size-5" />
+          </Button>
+        </Link>
+      </div>
 
-          <Table.Body>
-            {projects.map((project, index) => {
-              const isLast = index === projects.length - 1;
+      <Table>
+        <Table.Head columns={columns} />
 
-              return (
-                <Table.Row key={project.id} isLast={isLast}>
-                  <Table.Data>{project.name}</Table.Data>
-                  <Table.Data>{project.description}</Table.Data>
-                  <Table.Data>{formatDate(project.createdAt)}</Table.Data>
-                  <Table.Data>{formatDate(project.updatedAt)}</Table.Data>
+        <Table.Body>
+          {projects.map((project, index) => {
+            const isLast = index === projects.length - 1;
 
-                  <Table.Data>
-                    <div className="flex items-center justify-center">
-                      <Link href={`/dash/projects/${project.id}`}>
-                        <IconButton icon={ChevronRight} />
-                      </Link>
-                    </div>
-                  </Table.Data>
-                </Table.Row>
-              );
-            })}
-          </Table.Body>
-        </Table>
-      </section>
-    </Suspense>
+            return (
+              <Table.Row key={project.id} isLast={isLast}>
+                <Table.Data>{project.name}</Table.Data>
+                <Table.Data>{project.description}</Table.Data>
+                <Table.Data>{formatDate(project.createdAt)}</Table.Data>
+                <Table.Data>{formatDate(project.updatedAt)}</Table.Data>
+
+                <Table.Data>
+                  <div className="flex items-center justify-center">
+                    <Link href={`/dash/projects/${project.id}`}>
+                      <IconButton icon={ChevronRight} />
+                    </Link>
+                  </div>
+                </Table.Data>
+              </Table.Row>
+            );
+          })}
+        </Table.Body>
+      </Table>
+    </section>
   );
 }
--- src/app/(pages)/dash/projects/page.tsx
+++ src/app/(pages)/dash/projects/page.tsx
@@ -1,16 +1,28 @@
+import { Suspense } from 'react';
 import { redirect } from 'next/navigation';
 
 import { fetchUserSession } from '@app/actions';
-import { Header, List } from './_components';
+import { Header, List, LoadingList } from './_components';
 
 export default async function ProjectsPage() {
   const session = await fetchUserSession();
   if (!session || !session.user) return redirect('/auth');
 
+  const columns = [
+    { id: 'name', children: 'Nome' },
+    { id: 'description', children: 'Descrição' },
+    { id: 'createdAt', children: 'Criação em' },
+    { id: 'updatedAt', children: 'Última atualização em' },
+    { id: 'details', children: 'Detalhes', center: true },
+  ];
+
   return (
     <>
       <Header />
-      <List session={session} />
+
+      <Suspense fallback={<LoadingList columns={columns} />}>
+        <List session={session} columns={columns} />
+      </Suspense>
     </>
   );
 }
