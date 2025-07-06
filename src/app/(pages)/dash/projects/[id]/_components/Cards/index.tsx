96cb481b3ec3c12ed525122b6ad330b824954992
NEVI0 <neviocostamagagnin@gmail.com>
2025-07-06 20:00:35 -0300

    feat: update Cards component to include last updated date and enhance repository link functionality

--- src/app/(pages)/dash/projects/[id]/_components/Cards/index.tsx
+++ src/app/(pages)/dash/projects/[id]/_components/Cards/index.tsx
@@ -1,4 +1,4 @@
-import { Book, Calendar, Code2 } from 'lucide-react';
+import { Calendar, Code2 } from 'lucide-react';
 
 import { ProjectAbstract } from '@domain/entities';
 import { formatDate } from '@app/helpers';
@@ -14,13 +14,20 @@ export default function Cards({ project }: CardsProps) {
     <section className="flex items-center justify-between gap-4">
       <Card
         title="Repositório"
-        value="https://github.com/user/repo"
+        value="Acessar repositório"
+        valueLink={project.repositoryUrl}
         icon={Code2}
       />
-      <Card title="Última versão" value="v1.2.4" icon={Book} />
+
       <Card
         title="Criado em"
         value={formatDate(project.createdAt, 'DD of MMMM of YYYY')}
+        icon={Calendar}
+      />
+
+      <Card
+        title="Última atualização em"
+        value={formatDate(project.updatedAt, 'DD of MMMM of YYYY')}
         icon={Calendar}
       />
     </section>
