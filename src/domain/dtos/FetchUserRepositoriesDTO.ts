7023688ade3d0dfdbe4da2b393deff6691b2fc0b
NEVI0 <neviocostamagagnin@gmail.com>
2025-07-06 10:33:45 -0300

    feat: add user parameter to FetchUserRepositoriesDTO and update execute method in FetchUserRepositoriesUseCase for improved repository fetching

--- src/domain/dtos/FetchUserRepositoriesDTO.ts
+++ src/domain/dtos/FetchUserRepositoriesDTO.ts
@@ -1,4 +1,5 @@
 export default interface FetchUserRepositoriesDTO {
   token: string;
+  user: string;
   search?: string;
 }
--- src/domain/useCases/FetchUserRepositoriesUseCase.ts
+++ src/domain/useCases/FetchUserRepositoriesUseCase.ts
@@ -7,6 +7,6 @@ export default class FetchUserRepositoriesUseCase {
   ) {}
 
   public async execute(dto: FetchUserRepositoriesDTO) {
-    return this.userRepoRepository.findAll(dto.token, dto.search);
+    return this.userRepoRepository.findAll(dto.token, dto.user, dto.search);
   }
 }
