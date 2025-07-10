04b3998e5004fc81fed77a4c031c84fe17d2e64e
NEVI0 <neviocostamagagnin@gmail.com>
2025-07-10 18:48:07 -0300

    chore: remove authentication pages and components for a cleaner codebase

--- src/app/(pages)/auth/_components/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as AuthProviders } from './AuthProviders';
-export { default as Header } from './Header';
-export { default as OrSection } from './OrSection';
-export { default as TopLogo } from './TopLogo';
