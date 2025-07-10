15df4cb3bfc70cc2bcbeccbb772ebb78ba1c89fe
NEVI0 <neviocostamagagnin@gmail.com>
2025-07-10 18:17:13 -0300

    feat: add wait helper function for delayed execution

--- src/app/helpers/index.ts
+++ src/app/helpers/index.ts
@@ -4,3 +4,4 @@ export { default as formatToCurrency } from './formatToCurrency';
 export { default as generateRandomUUID } from './generateRandomUUID';
 export { default as getAppUrl } from './getAppUrl';
 export { default as handleError } from './handleError';
+export { default as wait } from './wait';
