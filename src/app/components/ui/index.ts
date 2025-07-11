69b7f08e1963c4dffc1906bb0e4ebc4276d61ee8
NEVI0 <neviocostamagagnin@gmail.com>
2025-07-10 21:57:28 -0300

    feat: add Spinner component for loading indication in UI

--- src/app/components/ui/index.ts
+++ src/app/components/ui/index.ts
@@ -8,6 +8,7 @@ export { default as Input } from './Input';
 export { default as Modal } from './Modal';
 export { default as Select } from './Select';
 export { default as Skeleton } from './Skeleton';
+export { default as Spinner } from './Spinner';
 export { default as Switch } from './Switch';
 export { default as Table } from './Table';
 export { default as Textarea } from './Textarea';
