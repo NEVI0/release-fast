003d0d8d7d7a6da6903684ee894114e361b92983
NEVI0 <neviocostamagagnin@gmail.com>
2025-07-12 13:48:47 -0300

    refactor: remove validation schema from updateProjectAction and updateReleaseAction for streamlined input handling

--- src/app/actions/updateProjectAction.ts
+++ src/app/actions/updateProjectAction.ts
@@ -1,19 +1,10 @@
 'use server';
 
-import { z } from 'zod';
-
 import { UpdateProjectDTO } from '@domain/dtos';
 import makeUpdateProjectUseCase from '@factories/useCases/makeUpdateProjectUseCase';
 
-const schema = z.object({
-  id: z.string().min(1, 'Project ID is required'),
-  name: z.string().min(1, 'Name is required'),
-  description: z.string(),
-});
-
-export default async function updateProjectAction(params: UpdateProjectDTO) {
+export default async function updateProjectAction(dto: UpdateProjectDTO) {
   try {
-    const dto = schema.parse(params);
     const project = await makeUpdateProjectUseCase().execute(dto);
 
     if (!project) throw new Error();
--- src/app/actions/updateReleaseAction.ts
+++ src/app/actions/updateReleaseAction.ts
@@ -1,21 +1,10 @@
 'use server';
 
-import { z } from 'zod';
-
 import { UpdateReleaseDTO } from '@domain/dtos';
 import makeUpdateReleaseUseCase from '@factories/useCases/makeUpdateReleaseUseCase';
 
-const schema = z.object({
-  id: z.string().min(1, 'Release ID is required'),
-  title: z.string().min(1, 'Title is required'),
-  shortDescription: z.string().min(1, 'Short description is required'),
-  fullDescription: z.string().min(1, 'Full description is required'),
-  version: z.string().min(1, 'Version is required'),
-});
-
-export default async function updateReleaseAction(params: UpdateReleaseDTO) {
+export default async function updateReleaseAction(dto: UpdateReleaseDTO) {
   try {
-    const dto = schema.parse(params);
     const release = await makeUpdateReleaseUseCase().execute(dto);
 
     if (!release) throw new Error();
