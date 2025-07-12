866f6ce604ea24d5e761c4518bb2cb8432e4f3ba
NEVI0 <neviocostamagagnin@gmail.com>
2025-07-12 11:05:09 -0300

    refactor: update formatToCurrency function to use dynamic locale mapping based on currency type

--- src/app/helpers/formatToCurrency.ts
+++ src/app/helpers/formatToCurrency.ts
@@ -1,10 +1,16 @@
 type CurrencyType = 'BRL' | 'USD' | 'EUR';
 
+const FORMAT_BY_CURRENCY: Record<CurrencyType, string> = {
+  BRL: 'pt-BR',
+  USD: 'en-US',
+  EUR: 'eur',
+};
+
 export default function formatToCurrency(
   value: number,
-  currency: CurrencyType = 'BRL'
+  currency: CurrencyType = 'USD'
 ) {
-  return new Intl.NumberFormat('pt-BR', {
+  return new Intl.NumberFormat(FORMAT_BY_CURRENCY[currency], {
     style: 'currency',
     currency,
   }).format(value);
