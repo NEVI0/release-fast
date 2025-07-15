80c1e3a0ee15ceb53c2d79e0d36649499cdc47f1
NEVI0 <neviocostamagagnin@gmail.com>
2025-07-14 23:16:15 -0300

    feat: add error and success animations to the animation constants

--- src/app/constants/animations.ts
+++ src/app/constants/animations.ts
@@ -1,9 +1,13 @@
 import githubAnimation from '@app/assets/animations/github.json';
 import emptyAnimation from '@app/assets/animations/empty.json';
+import errorAnimation from '@app/assets/animations/error.json';
+import successAnimation from '@app/assets/animations/success.json';
 import notFoundAnimation from '@app/assets/animations/not-found.json';
 
 export const ANIMATIONS = {
   GITHUB: githubAnimation,
   EMPTY: emptyAnimation,
   NOT_FOUND: notFoundAnimation,
+  ERROR: errorAnimation,
+  SUCCESS: successAnimation,
 };
