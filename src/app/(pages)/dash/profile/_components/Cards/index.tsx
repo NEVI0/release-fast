2fbd7e75d0dba860ea3c95805f88b1e523e8c712
NEVI0 <neviocostamagagnin@gmail.com>
2025-07-18 18:43:11 -0300

    refactor: simplify Cards component layout and improve email display logic

--- src/app/(pages)/dash/profile/_components/Cards/index.tsx
+++ src/app/(pages)/dash/profile/_components/Cards/index.tsx
@@ -20,36 +20,20 @@ export default function Cards({ user }: CardsProps) {
         )}/m`;
 
   return (
-    <section className="flex flex-col gap-4">
-      <div className="flex items-center justify-between gap-4">
-        <Card title="Current plan" value={currentPlan} icon={BadgeCheck} />
-
-        <Card title="Next due date" value="June 20, 2025" icon={Calendar} />
-
-        <Card
-          title="Created at"
-          value={formatDate(user.createdAt, 'MMMM DD, YYYY')}
-          icon={Calendar}
-        />
-      </div>
+    <section className="flex items-center gap-4">
+      <Card title="Current plan" value={currentPlan} icon={BadgeCheck} />
 
       <Card
         title="Account e-mail"
         value={user.email || 'E-mail not provided'}
         icon={Mail}
-      >
-        {!user.email ? (
-          <Button variant="primary">
-            Provide e-mail <Button.Icon icon={Plus} />
-          </Button>
-        ) : !user.emailVerified ? (
-          <Button variant="primary">
-            Verify e-mail <Button.Icon icon={MailCheck} />
-          </Button>
-        ) : (
-          <Badge variant="success">E-mail verified</Badge>
-        )}
-      </Card>
+      />
+
+      <Card
+        title="Created at"
+        value={formatDate(user.createdAt, 'MMMM DD, YYYY')}
+        icon={Calendar}
+      />
     </section>
   );
 }
