78c111df1b9b05ade513373e5882b36cdab5bf46
NEVI0 <neviocostamagagnin@gmail.com>
2025-07-19 11:21:21 -0300

    refactor: remove error handling from StripePaymentProvider methods for cleaner code

--- src/infra/providers/StripePaymentProvider.ts
+++ src/infra/providers/StripePaymentProvider.ts
@@ -27,75 +27,59 @@ export default class StripePaymentProvider implements PaymentProviderAbstract {
   private readonly stripe = stripe;
 
   public async createUser(user: PaymentUserAbstract) {
-    try {
-      const customer = await this.stripe.customers.create({
-        name: user.name,
-        email: user.email,
-        metadata: {
-          userId: user.id,
-        },
-      });
+    const customer = await this.stripe.customers.create({
+      name: user.name,
+      email: user.email,
+      metadata: {
+        userId: user.id,
+      },
+    });
 
-      return new PaymentUser({
-        id: customer.id,
-        name: customer.name || user.name,
-        email: customer.email || user.email,
-      });
-    } catch (error) {
-      return null;
-    }
+    return new PaymentUser({
+      id: customer.id,
+      name: customer.name || user.name,
+      email: customer.email || user.email,
+    });
   }
 
   public async createCheckout(params: CreateCheckoutParams) {
-    try {
-      if (params.plan === 'free') throw new Error();
+    if (params.plan === 'free') throw new Error();
 
-      const session = await stripe.checkout.sessions.create({
-        customer: params.user.paymentId,
-        client_reference_id: params.user.id,
-        line_items: [
-          {
-            price: PRICE_ID_BY_PLAN[params.plan],
-            quantity: 1,
-          },
-        ],
-        mode: 'subscription',
-        payment_method_types: ['card'],
-        success_url: params.url.success,
-        cancel_url: params.url.cancel,
-        metadata: params.metadata,
-      });
+    const session = await stripe.checkout.sessions.create({
+      customer: params.user.paymentId,
+      client_reference_id: params.user.id,
+      line_items: [
+        {
+          price: PRICE_ID_BY_PLAN[params.plan],
+          quantity: 1,
+        },
+      ],
+      mode: 'subscription',
+      payment_method_types: ['card'],
+      success_url: params.url.success,
+      cancel_url: params.url.cancel,
+      metadata: params.metadata,
+    });
 
-      return { id: session.id };
-    } catch (error) {
-      return null;
-    }
+    return { id: session.id };
   }
 
   public async createPortal(params: CreatePortalParams) {
-    try {
-      const portal = await this.stripe.billingPortal.sessions.create({
-        customer: params.user.paymentId,
-        return_url: params.return.url,
-      });
+    const portal = await this.stripe.billingPortal.sessions.create({
+      customer: params.user.paymentId,
+      return_url: params.return.url,
+    });
 
-      return { url: portal.url };
-    } catch (error) {
-      return null;
-    }
+    return { url: portal.url };
   }
 
   public createEvent(params: CreateEventParams) {
-    try {
-      const event = this.stripe.webhooks.constructEvent(
-        params.body,
-        params.signature,
-        params.secret
-      );
+    const event = this.stripe.webhooks.constructEvent(
+      params.body,
+      params.signature,
+      params.secret
+    );
 
-      return event;
-    } catch (error) {
-      return null;
-    }
+    return event;
   }
 }
