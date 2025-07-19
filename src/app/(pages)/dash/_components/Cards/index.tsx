1eb3e8461a4bdeffcef6b84197384fa6a29dff46
NEVI0 <neviocostamagagnin@gmail.com>
2025-07-19 19:19:13 -0300

    feat: update dashboard cards to reflect new project metrics and icons

--- src/app/(pages)/dash/_components/Cards/index.tsx
+++ src/app/(pages)/dash/_components/Cards/index.tsx
@@ -1,13 +1,13 @@
-import { Bell, Boxes, BrainCircuit } from 'lucide-react';
+import { Boxes, BrainCircuit, Cable } from 'lucide-react';
 
 import { Card } from '@app/components/common';
 
 export default function Cards() {
   return (
     <section className="flex items-center justify-between gap-4">
-      <Card title="Active projects" value="3" icon={Boxes} />
-      <Card title="Notifications sent" value="223" icon={Bell} />
+      <Card title="Projects created" value="3" icon={Boxes} />
       <Card title="Releases generated" value="100" icon={BrainCircuit} />
+      <Card title="Account provider" value="GitHub" icon={Cable} />
     </section>
   );
 }
