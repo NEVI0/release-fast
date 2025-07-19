aa46f9850fce9e7892b1957c7103df4cb784b5aa
NEVI0 <neviocostamagagnin@gmail.com>
2025-07-19 14:32:21 -0300

    feat: add construction animation and integrate into ANIMATIONS constant

--- src/app/constants/animations.ts
+++ src/app/constants/animations.ts
@@ -1,3 +1,4 @@
+import constructionAnimation from '@app/assets/animations/construction.json';
 import githubAnimation from '@app/assets/animations/github.json';
 import emptyAnimation from '@app/assets/animations/empty.json';
 import errorAnimation from '@app/assets/animations/error.json';
@@ -5,6 +6,7 @@ import successAnimation from '@app/assets/animations/success.json';
 import notFoundAnimation from '@app/assets/animations/not-found.json';
 
 export const ANIMATIONS = {
+  CONSTRUCTION: constructionAnimation,
   GITHUB: githubAnimation,
   EMPTY: emptyAnimation,
   NOT_FOUND: notFoundAnimation,
