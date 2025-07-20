ac18d213e2b1ddf4dd79f0ba8ba36c3997d0ffbc
NEVI0 <neviocostamagagnin@gmail.com>
2025-07-20 16:39:19 -0300

    refactor: remove useWindowSize hook to streamline hooks index

--- src/app/hooks/index.ts
+++ src/app/hooks/index.ts
@@ -1,4 +1,3 @@
 export { default as useForm } from './useForm';
 export { default as useStripe } from './useStripe';
 export { default as useToast } from './useToast';
-export { default as useWindowSize } from './useWindowSize';
