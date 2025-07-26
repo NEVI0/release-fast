50a34bd202b876d9bfa5eb6711cc0607ad0ebe1e
NEVI0 <neviocostamagagnin@gmail.com>
2025-07-26 19:51:51 -0300

    refactor: simplify Video component by removing fullscreen functionality and updating layout

--- src/app/(pages)/(home)/_components/HowItWorks/components/Video/index.tsx
+++ src/app/(pages)/(home)/_components/HowItWorks/components/Video/index.tsx
@@ -1,42 +1,18 @@
 'use client';
 
-import { useRef } from 'react';
-import Image from 'next/image';
-
 export default function Video() {
-  const videoRef = useRef<HTMLVideoElement | null>(null);
-
-  function handleClickVideo() {
-    if (!videoRef.current) return;
-
-    videoRef.current.requestFullscreen();
-    videoRef.current.play();
-  }
-
   return (
-    <>
-      <div
-        className="w-full h-[224px] md:w-[832px] md:h-[432px] bg-text-primary rounded-3xl flex items-center justify-center overflow-hidden cursor-pointer"
-        onClick={handleClickVideo}
-      >
-        <Image
-          src="/images/video-thumbnail.png"
-          width={832}
-          height={432}
-          alt="Video Thumbnail"
-        />
-      </div>
-
+    <div className="flex w-[324px] h-[182px] md:w-[832px] md:h-[468px] bg-text-primary items-center justify-center overflow-hidden cursor-pointer border border-border">
       <video
-        ref={videoRef}
         id="video"
         controls
+        className="w-[324px] h-[182px] md:w-[832px] md:h-[468px]"
         preload="none"
-        className="absolute top-[-1000rem] right-[-1000rem] z-[-1000]"
+        poster="/images/video-thumbnail.png"
       >
         <source src="/videos/how-it-works-v1.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-    </>
+    </div>
   );
 }
