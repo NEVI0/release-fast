d24ccd4a7d699d1ecfb48d68b548375f261a3a34
NEVI0 <neviocostamagagnin@gmail.com>
2025-07-28 22:47:56 -0300

    feat: enhance AI prompt generation with structured output for change logs

--- src/app/(pages)/dash/projects/[id]/create-release/_components/Form/helpers/getAiPrompt.ts
+++ src/app/(pages)/dash/projects/[id]/create-release/_components/Form/helpers/getAiPrompt.ts
@@ -4,5 +4,10 @@ export default function getAiPrompt(diff: string) {
   
   After that generate a short change log and a full change log about it for the final user of the projetc analized,
   and ONLY return it as an array containing the two description as elements to me so I can set my internal variables easily.
+
+  Rules:
+  - The first array element (the short description) must be a simple string;
+  - The second array element (the full description) can be generated as a string with markdown characters;
+  - Generate the two description in English;
   `;
 }
