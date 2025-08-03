bf3f5e50e1bc10073c352f2a8862cef6606f8ee3
NEVI0 <neviocostamagagnin@gmail.com>
2025-08-03 11:43:06 -0300

    feat: SEO improvents

--- src/app/(pages)/(home)/privacy/page.tsx
+++ src/app/(pages)/(home)/privacy/page.tsx
@@ -190,7 +190,7 @@ export default function PrivacyPage() {
       </div>
 
       <small className="text-sm text-text-secondary">
-        Last updated: {formatDate(new Date('07-12-2025'), 'MMMM DD, YYYY')}
+        Last updated: {formatDate(new Date('08-03-2025'), 'MMMM DD, YYYY')}
       </small>
     </>
   );
