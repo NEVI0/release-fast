fdff31931584c3e86b3324072cec9ca0db5911a9
NEVI0 <neviocostamagagnin@gmail.com>
2025-08-23 14:32:43 -0300

    refactor: replace GitHub sign-in button with a link to access the dashboard

--- src/app/components/common/Header/components/LoginLink/index.tsx
+++ src/app/components/common/Header/components/LoginLink/index.tsx
@@ -1,14 +1,12 @@
-'use client';
-
-import { accessAccountAction } from '@app/actions';
+import Link from 'next/link';
 
 export default function LoginLink() {
   return (
-    <button
+    <Link
+      href="/access"
       className="cursor-pointer h-full flex items-center justify-center font-semibold underline text-primary"
-      onClick={() => accessAccountAction('github')}
     >
-      Sign in with GitHub
-    </button>
+      Access Dashboard
+    </Link>
   );
 }
