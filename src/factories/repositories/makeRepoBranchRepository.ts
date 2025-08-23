b4c984c038f52676989218b58e0755c3bced0070
NEVI0 <neviocostamagagnin@gmail.com>
2025-08-23 14:31:55 -0300

    feat: refactor repository factory functions to support GitLab integration

--- src/factories/repositories/makeRepoBranchRepository.ts
+++ src/factories/repositories/makeRepoBranchRepository.ts
@@ -1,7 +1,10 @@
 import { SessionProvider } from '@domain/entities';
+import { HttpProviderAbstract } from '@domain/providers';
 import { RepoBranchRepositoryAbstract } from '@domain/repositories';
 
 import GithubRepoBranchRepository from '@infra/repositories/GithubRepoBranchRepository';
+import GitlabRepoBranchRepository from '@infra/repositories/GitlabRepoBranchRepository';
+
 import makeHttpProvider from '@factories/providers/makeHttpProvider';
 
 interface Params {
@@ -11,10 +14,18 @@ interface Params {
 
 let instance: RepoBranchRepositoryAbstract | null = null;
 
+const INSTANCES: Record<
+  SessionProvider,
+  (httpProvider: HttpProviderAbstract) => RepoBranchRepositoryAbstract
+> = {
+  github: (httpProvider) => new GithubRepoBranchRepository(httpProvider),
+  gitlab: (httpProvider) => new GitlabRepoBranchRepository(httpProvider),
+};
+
 export default function makeRepoBranchRepository({ provider, token }: Params) {
   if (!instance) {
     const httpProvider = makeHttpProvider({ provider, token });
-    instance = new GithubRepoBranchRepository(httpProvider);
+    instance = INSTANCES[provider](httpProvider);
   }
 
   return instance;
