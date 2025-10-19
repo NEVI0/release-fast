0c1e95a19d3fa7988c88f7b0d4234f4e345390df
NEVI0 <neviocostamagagnin@gmail.com>
2025-10-19 19:09:24 -0300

    test: add unit tests for FetchReleaseByIdUseCase to validate release retrieval and error handling

---
+++ __tests__/domain/useCases/FetchReleaseByIdUseCase.test.ts
@@ -0,0 +1,46 @@
+import { faker } from '@faker-js/faker';
+
+import FetchReleaseByIdUseCase from '@domain/useCases/FetchReleaseByIdUseCase';
+
+import { FakeRelease } from '@tests/fakers/entities';
+import { FakeReleaseRepository } from '@tests/fakers/repositories';
+
+const makeUseCase = () => {
+  const repository = new FakeReleaseRepository();
+  const useCase = new FetchReleaseByIdUseCase(repository);
+
+  return { repository, useCase };
+};
+
+describe('FetchReleaseByIdUseCase', () => {
+  it('should return release by ID', async () => {
+    const { repository, useCase } = makeUseCase();
+
+    const id = faker.string.uuid();
+
+    repository.mockRelease(new FakeRelease({ id }));
+    repository.mockRelease(new FakeRelease({}));
+
+    const release = await useCase.execute({ id });
+
+    expect(release?.id).toBe(id);
+  });
+
+  it('should return "null" to release not found', async () => {
+    const { repository, useCase } = makeUseCase();
+
+    repository.mockRelease(new FakeRelease({}));
+    repository.mockRelease(new FakeRelease({}));
+
+    const release = await useCase.execute({ id: faker.string.uuid() });
+
+    expect(release).toBeNull();
+  });
+
+  it('should throw an error if "id" is missing', async () => {
+    const { useCase } = makeUseCase();
+    await expect(useCase.execute({ id: '' })).rejects.toThrow(
+      'The release ID is required'
+    );
+  });
+});
