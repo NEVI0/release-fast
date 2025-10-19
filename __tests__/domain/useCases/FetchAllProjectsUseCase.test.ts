e24b53c0cb8caa77cbdb2ccb2f94e291eca1fa7f
NEVI0 <neviocostamagagnin@gmail.com>
2025-10-19 15:48:14 -0300

    test: add unit tests for FetchAllProjectsUseCase to validate project retrieval and error handling

---
+++ __tests__/domain/useCases/FetchAllProjectsUseCase.test.ts
@@ -0,0 +1,39 @@
+import { faker } from '@faker-js/faker';
+
+import FetchAllProjectsUseCase from '@domain/useCases/FetchAllProjectsUseCase';
+
+import { FakeProject } from '@tests/fakers/entities';
+import { FakeProjectRepository } from '@tests/fakers/repositories';
+
+const makeUseCase = () => {
+  const repository = new FakeProjectRepository();
+  const useCase = new FetchAllProjectsUseCase(repository);
+
+  return { repository, useCase };
+};
+
+describe('FetchAllProjectsUseCase', () => {
+  it("should return all user's projects correctly", async () => {
+    const { repository, useCase } = makeUseCase();
+
+    const userId = faker.string.uuid();
+
+    repository.mockProject(new FakeProject({ userId }));
+    repository.mockProject(new FakeProject({ userId }));
+    repository.mockProject(new FakeProject({}));
+
+    const projects = await useCase.execute({ userId });
+
+    expect(projects).toHaveLength(2);
+    projects.forEach((project) => {
+      expect(project.userId).toBe(userId);
+    });
+  });
+
+  it('should throw an error if "id" is missing', async () => {
+    const { useCase } = makeUseCase();
+    await expect(useCase.execute({ userId: '' })).rejects.toThrow(
+      'The user ID is required'
+    );
+  });
+});
