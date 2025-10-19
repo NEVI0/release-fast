44c4786afd04a0e6c7b3033beb741327f638e8e4
NEVI0 <neviocostamagagnin@gmail.com>
2025-10-19 15:38:19 -0300

    test: add unit tests for DeleteReleaseByIdUseCase to validate release deletion and error handling

---
+++ __tests__/domain/useCases/DeleteReleaseByIdUseCase.test.ts
@@ -0,0 +1,36 @@
+import { faker } from '@faker-js/faker';
+
+import DeleteReleaseByIdUseCase from '@domain/useCases/DeleteReleaseByIdUseCase';
+
+import { FakeRelease } from '@tests/fakers/entities';
+import { FakeReleaseRepository } from '@tests/fakers/repositories';
+
+const makeUseCase = () => {
+  const repository = new FakeReleaseRepository();
+  const useCase = new DeleteReleaseByIdUseCase(repository);
+
+  return { repository, useCase };
+};
+
+describe('DeleteReleaseByIdUseCase', () => {
+  it('should delete release correctly', async () => {
+    const { repository, useCase } = makeUseCase();
+
+    const id = faker.string.uuid();
+
+    repository.mockRelease(new FakeRelease({ id }));
+    repository.mockRelease(new FakeRelease({}));
+
+    await useCase.execute({ id });
+    const release = await repository.findById(id);
+
+    expect(release).toBeNull();
+  });
+
+  it('should throw an error if "id" is missing', async () => {
+    const { useCase } = makeUseCase();
+    await expect(useCase.execute({ id: '' })).rejects.toThrow(
+      'The release ID is required'
+    );
+  });
+});
