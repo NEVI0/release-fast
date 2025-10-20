dcac9d79f4316ddc0803f287f41005d56c3fa9f0
NEVI0 <neviocostamagagnin@gmail.com>
2025-10-19 22:35:01 -0300

    test: refactor FakePaymentProvider methods to use jest.fn for improved mock functionality

--- __tests__/fakers/providers/FakePaymentProvider.ts
+++ __tests__/fakers/providers/FakePaymentProvider.ts
@@ -3,21 +3,25 @@ import { faker } from '@faker-js/faker/locale/en';
 import { PaymentProviderAbstract } from '@domain/providers';
 
 export default class FakePaymentProvider implements PaymentProviderAbstract {
-  public createCheckout: PaymentProviderAbstract['createCheckout'] = async (
-    _
-  ) => {
-    return { id: faker.string.uuid() };
-  };
+  public createCheckout: PaymentProviderAbstract['createCheckout'] = jest.fn(
+    async (_) => {
+      return { id: faker.string.uuid() };
+    }
+  );
 
-  public createUser: PaymentProviderAbstract['createUser'] = async (user) => {
-    return user;
-  };
+  public createUser: PaymentProviderAbstract['createUser'] = jest.fn(
+    async (user) => {
+      return user;
+    }
+  );
 
-  public createPortal: PaymentProviderAbstract['createPortal'] = async (_) => {
-    return { url: faker.internet.url() };
-  };
+  public createPortal: PaymentProviderAbstract['createPortal'] = jest.fn(
+    async (_) => {
+      return { url: faker.internet.url() };
+    }
+  );
 
-  public createEvent: PaymentProviderAbstract['createEvent'] = (_) => {
+  public createEvent: PaymentProviderAbstract['createEvent'] = jest.fn((_) => {
     return null;
-  };
+  });
 }
